5489053fefd85eb89717621878cfa9e1a52d48ce
Jonathan Scholtes <joscholt@microsoft.com>
2019-07-18 18:14:44 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+
+ Please see https://stochasticcoder.com
---
+++ whl/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "DemoWheel",
+    author = "Jonathan Scholtes",
+    author_email = "Jonathan@Stochasticcoder.com",
+    version = "0.1",
+    packages = find_packages())
